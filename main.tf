12ddb4841a5686f90cdc0a468efb05b3f8e231cd
Uday Kumar Gunda <gundaudaykumar100@gmail.com>
2025-11-30 12:06:59 +0530

    added new terraform file

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = " >= 1.11.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "vm" {
+  ami           = "ami-02b8269d5e85954ef"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ec2_instance_vm"
+  }
+}
